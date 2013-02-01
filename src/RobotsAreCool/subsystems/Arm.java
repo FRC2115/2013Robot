da8619fdb8c22c1f1af2938ba2d99a4b681a48cd
Jordan Giacone <Jmgiacone@comcast.net>
2013-02-01 17:05:07 -0600

    Method changes
    Once again, Arms have no solenoids, that's the job of the Plunger

--- src/RobotsAreCool/subsystems/Arm.java
+++ src/RobotsAreCool/subsystems/Arm.java
@@ -2,29 +2,21 @@ package RobotsAreCool.subsystems;
 
 import RobotsAreCool.Templates.RobotMap;
 import edu.wpi.first.wpilibj.Jaguar;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Arm extends Subsystem 
 {
     private Jaguar j;
-    private Solenoid s;
     
     public Arm()
     {
         j = new Jaguar(RobotMap.ARM_MOTOR_CHANNEL);
-        s = new Solenoid(RobotMap.ARM_SOLENOID_CHANNEL);
     }
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-    public void setSolenoid(boolean n)
-    {
-        s.set(n);
-    }
-    
-    public void setJaguarSpeed(double x)
+    public void set(double x)
     {
         j.set(x);
     }
