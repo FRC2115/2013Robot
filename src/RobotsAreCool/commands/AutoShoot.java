0474096c55de6667d4a03fa9b623a04bc793e321
Jordan Giacone <Jmgiacone@comcast.net>
2013-02-01 16:59:13 -0600

    Fixed Coding Style

--- src/RobotsAreCool/commands/AutoShoot.java
+++ src/RobotsAreCool/commands/AutoShoot.java
@@ -8,35 +8,45 @@ package RobotsAreCool.commands;
  *
  * @author Tripp
  */
-public class AutoShoot extends CommandBase {
+public class AutoShoot extends CommandBase 
+{
     
     public AutoShoot() 
     {
         requires(shooter);
+        requires(camera);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize() 
+    {
         
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    protected void execute() 
+    {
+        //TODO Put autoshoot code here
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    protected boolean isFinished()
+    {
         return false;
     }
 
     // Called once after isFinished returns true
-    protected void end() {
+    protected void end() 
+    {
+        
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() {
+    protected void interrupted() 
+    {
+        
     }
 }
